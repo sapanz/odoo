28b4f164ca1335f0f47e01b29be766913e54bcb0
Nisha patel <nit@odoo.com>
2020-09-08 18:56:54 +0530

    [FIX] hr_holidays: Fix possible leaks for leave reason in several model
    Currently, no protection against following models.
    - resource.calendar.leaves: created using leave.name -> accessible for example
      freely to project.user, mrp.user
    - hr.leaves.report: no protection against reading name , accessible to all
      (no group)
    
    So in this commit, Improved leave reason display for several user's
    
    1) resource.calendar.leaves:
       - Apply group holiday All approver on field name so it should
    not be read by the all the user.
       - Added the new field public_name, it will display the leave name only if:
          - resource leave is not related to time off
          - current user is time off officer
          - manager of that time off is current user
          - time off of the current user
    otherwise display the '*****'
    
    2) hr.leave.report:
       - Internal user can read their own time off using stat button from the
    employee form view and user's profile.
       - Time off All approver can read the all user's time off using the
    button from the employee form view,
      - Time off manager can read the whole report from the time off menu.
    
    TaskID: 2314838

--- addons/hr_holidays/models/resource.py
+++ addons/hr_holidays/models/resource.py
@@ -1,10 +1,47 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.osv import expression
 
 
 class CalendarLeaves(models.Model):
     _inherit = "resource.calendar.leaves"
+    _rec_name = "public_name"
 
     holiday_id = fields.Many2one("hr.leave", string='Leave Request')
+    # Override name field and apply a group to restrict the reason of leave
+    name = fields.Char(groups='hr_holidays.group_hr_holidays_user')
+    public_name = fields.Char('Leave Reason', compute='_compute_public_name', inverse='_inverse_public_name', search='_search_public_name')
+
+    @api.depends_context('uid')
+    def _compute_public_name(self):
+        self.check_access_rights('read')
+        self.check_access_rule('read')
+
+        is_officer = self.user_has_groups('hr_holidays.group_hr_holidays_user')
+
+        for leave in self:
+            holiday = leave.sudo().holiday_id
+            if not holiday or is_officer or leave.resource_id.user_id == self.env.user or holiday.manager_id == self.env.user:
+                leave.public_name = leave.sudo().name
+            else:
+                leave.public_name = '*****'
+
+    def _inverse_public_name(self):
+        is_officer = self.user_has_groups('hr_holidays.group_hr_holidays_user')
+
+        for leave in self:
+            holiday = leave.sudo().holiday_id
+            if not holiday or is_officer or leave.resource_id.user_id == self.env.user or holiday.manager_id == self.env.user:
+                leave.sudo().name = leave.public_name
+
+    def _search_public_name(self, operator, value):
+        is_officer = self.user_has_groups('hr_holidays.group_hr_holidays_user')
+        domain = [('name', operator, value)]
+
+        if not is_officer:
+            domain = expression.AND([domain, [('resource_id.user_id', '=', self.env.uid)]])
+
+        leaves = self.search(domain)
+        return [('id', 'in', leaves.ids)]
