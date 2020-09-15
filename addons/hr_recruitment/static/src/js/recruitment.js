5e07e205f0b6939a158a926f44309bba73c63d62
Krupal Oza <koz@odoo.com>
2020-09-15 19:09:53 +0530

    [IMP] hr_recruitment: Recruitment Improvements for mobile
    Purpose of this task is to imrpove mobile interface for Recruitment Applications.
    
    In this commit, we improve the Recruitment applications in mobile interface by
    below changes.
     - In the job position kanban view :
        - Align the logo of Link Trackers with the job description.
        - Reduce the space between header and job description.
        - Make fullscreen dialog of Create a job position.
     - Change the font colour of dropdown menu in new application view.
     - Create new kanban view of Job sources.
    
    TaskID - 2333976

--- addons/hr_recruitment/static/src/js/recruitment.js
+++ addons/hr_recruitment/static/src/js/recruitment.js
@@ -16,3 +16,30 @@ odoo.define('job.update_kanban', function (require) {
         }
     });
 });
+
+odoo.define('hr_recruitment.hr_recruitment_kanban', function (require) {
+"use strict";
+
+    const config = require('web.config');
+    var FormView = require('web.FormView');
+    var FormRenderer = require('web.FormRenderer');
+    var viewRegistry = require('web.view_registry');
+
+    var RecruitmentFormRenderer = FormRenderer.extend({
+        on_attach_callback: function () {
+            var self = this;
+            self._super.apply(this, arguments);
+            if (!config.device.isMobile) {
+                return;
+            }
+            self.$el.closest('.modal').addClass('o_modal_full');
+        },
+    });
+    var RecruitmentFormView = FormView.extend({
+        config: _.extend({}, FormView.prototype.config, {
+            Renderer: RecruitmentFormRenderer
+        }),
+    });
+
+    viewRegistry.add('hr_recruitment_kanban', RecruitmentFormView);
+});
