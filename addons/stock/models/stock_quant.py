908961f42de3fd9699b78147151b9716f204ebde
Tiffany Chang (tic) <tic@odoo.com>
2020-09-22 15:02:10 +0200

    arm patch

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -268,7 +268,7 @@ class StockQuant(models.Model):
         self.env['stock.quant'].flush(['location_id', 'owner_id', 'package_id', 'lot_id', 'product_id'])
         self.env['product.product'].flush(['virtual_available'])
         removal_strategy = self._get_removal_strategy(product_id, location_id)
-        removal_strategy_order = self._get_removal_strategy_order(removal_strategy)
+        removal_strategy_order = self._get_removal_strategy_order('fifo')
         domain = [
             ('product_id', '=', product_id.id),
         ]
