5c0a288e55da85298d907b8b9aa017e4cfeb82bf
Géry Debongnie <ged@odoo.com>
2020-10-08 21:56:33 +0200

    remove Action component

--- addons/wowl/static/src/components/webclient/webclient.ts
+++ addons/wowl/static/src/components/webclient/webclient.ts
@@ -1,11 +1,10 @@
-import { Action } from "../action/action";
 import { ActionContainer } from "../../services/action_manager/action_manager";
 import { Component } from "@odoo/owl";
 import { NavBar } from "../navbar/navbar";
 import { OdooEnv } from "../../types";
 
 export class WebClient extends Component<{}, OdooEnv> {
-  static components = { Action, ActionContainer, NavBar };
+  static components = { ActionContainer, NavBar };
   static template = "wowl.WebClient";
   Components = this.env.registries.Components.getEntries();
 }
