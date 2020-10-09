e4b64de24ca6f6e3bc78075032c66421d6eced02
Achraf (abz) <abz@odoo.com>
2020-10-09 17:53:32 +0200

    [FIX] point_of_sale: Send invoice by email with qr_code
    Issue
    
        - Install "Point of Sale"
        - Enable "Qr Code" in Invoicing
        - Open new POS session
        - Set random customer
        - Try to send the invoice by email
    
    Cause
    
        The 'qr_code_urls' doesnt exist in the dictionary.
    
    Solution
    
        Add 'qr_code_urls' with all generated qr code urls to the dictionary in report.point_of_sale.report_invoice
    
    opw-2357088

--- addons/point_of_sale/report/pos_invoice.py
+++ addons/point_of_sale/report/pos_invoice.py
@@ -23,5 +23,15 @@ class PosInvoiceReport(models.AbstractModel):
             not_invoiced_posorders = PosOrder.browse(not_invoiced_orders_ids)
             not_invoiced_orders_names = [a.name for a in not_invoiced_posorders]
             raise UserError(_('No link to an invoice for %s.') % ', '.join(not_invoiced_orders_names))
+        docs = self.env['account.move'].sudo().browse(ids_to_print)
+        qr_code_urls = {}
+        for invoice in docs:
+            if invoice.display_qr_code:
+                new_code_url = invoice.generate_qr_code()
+                if new_code_url:
+                    qr_code_urls[invoice.id] = new_code_url
 
-        return {'docs': self.env['account.move'].sudo().browse(ids_to_print)}
+        return {
+            'docs': docs,
+            'qr_code_urls': qr_code_urls
+        }
