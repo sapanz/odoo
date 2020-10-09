e3e51388c7466937c3cfbeae2996772eb2ec06a5
Hiral Bhavsar <hbh@odoo.com>
2020-10-09 17:15:32 +0530

    [FIX] mail: marking activity done from form view
    Before this commit:
    
    When editing the activity, traceback while processing with buttons
    "Done & Schedule Next", "Mark as Done" from the form view.
    
    This is happening because when we process with these buttons it complete
    the action of respective buttons and then the trigger will go on the close
    window. Hence, we don't have activity data as "mark as done" remove
    the activity.
    
    After this commit:
    
    Footer buttons will be processed as expected without any traceback.
    
    Task-2357681

--- addons/mail/static/src/models/activity/activity.js
+++ addons/mail/static/src/models/activity/activity.js
@@ -148,8 +148,13 @@ function factory(dependencies) {
                 method: 'activity_format',
                 args: [this.id],
             }));
-            this.update(this.constructor.convertData(data));
-            this.thread.refresh();
+            if (data) {
+                this.update(this.constructor.convertData(data));
+                this.thread.refreshActivities();
+            } else {
+                this.thread.refresh();
+                this.delete();
+            }
         }
 
         /**
